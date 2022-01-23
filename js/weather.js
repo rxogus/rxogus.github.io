2adba27d4ae61b62bdab1d7c129669d83750f9a5
xoguss <97212309+rxogus@users.noreply.github.com>
2022-01-23 19:46:21 +0900

    mixed content

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "5a75c711281739da272722f27da3e25e";
 function getGeolocation(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather h2");
         weather.innerText = `${data.main.temp}℃ ${data.weather[0].main} ${data.name}`
